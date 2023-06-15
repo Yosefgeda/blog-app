4a36615f0bb5ebda40c448f889ad445307f2cd4c
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-06-15 10:17:42 +0300

    Make changes as per review

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :comments
-  has_many :likes
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
   has_many :posts, foreign_key: 'author_id'
 
   def three_recent_posts
